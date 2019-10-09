49fd2882a7561b77dcad2e0785648e9c42c50494
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2019-10-09 11:01:26 +0530

    Create K8sfile.js

---
+++ K8sfile.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var handleRequest = function(request, response) {
+  console.log('Recieved request for URL: ' + request.url);
+  response.writeHead(200);
+  response.write('*********************************\n');
+  response.write('* Greetings from the DEMO! *\n');
+  response.end(  '****************************\n');
+  
+  };
+  
+  var www = http.createServer(handleRequest);
+  www.listen(8000);
